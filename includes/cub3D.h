53a78f4594ad2b045a89888e9511b730ea385762
Kilian Tintim Goncalves <ktintim-@c2r5s3.42lausanne.ch>
2025-05-16 16:22:59 +0200

    modif

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ktintim- <ktintim-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:50:46 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/05/16 16:18:52 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/05/16 16:22:36 by ktintim-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "functions.h"
+# include "structs.h"
 # include "../minilibx-linux/mlx.h"
 # include "../libft/libft.h"
-# include "../includes/functions.h"
-# include "../includes/structs.h"
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
