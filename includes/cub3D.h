97d9d45925903bfbde86d6549099da25e40e6379
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-05-16 16:28:34 +0200

    change pth

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,14 +6,14 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:50:46 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/05/16 16:18:52 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/05/16 16:28:27 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "../minilibx-linux/mlx.h"
+# include "../minilibx/mlx.h"
 # include "../libft/libft.h"
 # include "../includes/functions.h"
 # include "../includes/structs.h"
