c54fff001cc3e409cb3a0d684660493705fca554
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-05-16 16:19:03 +0200

    update .h

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -3,16 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ktintim- <ktintim-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:50:46 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/05/16 15:41:27 by ktintim-         ###   ########.fr       */
+/*   Updated: 2025/05/16 16:18:52 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-#include "../minilibx-linux/mlx.h"
+# include "../minilibx-linux/mlx.h"
+# include "../libft/libft.h"
+# include "../includes/functions.h"
+# include "../includes/structs.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 #endif
