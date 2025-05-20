f318f8f491c15e3ce2aeebc2f3170a7c97777b91
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-05-20 15:36:09 +0200

    sort functions.c

--- includes/functions.h
+++ includes/functions.h
@@ -6,25 +6,28 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 15:46:40 by ktintim-          #+#    #+#             */
-/*   Updated: 2025/05/20 14:46:55 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/05/20 15:35:53 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FUNCTIONS_H
 # define FUNCTIONS_H
-
-
+//constructor.c
 void	window_constructor(t_window *window);
 void	texture_constructor(t_texture *texture);
 void	data_constructor(t_data *data);
-
+//parsing.c
 void	parsing(char *file, t_data *data);
-int		check_texture(t_texture *txtr);
+//checking_utils.c
+int		is_valid_map_char(char c);
+//parsing_utils.c
 void	strip_newline(char *str);
-void	free_array(char **tab);
-int		check_map(t_map *map);
 void	get_map_dimensions(char **lines, t_map *map);
 void	copy_map(char **lines, t_map *map);
-int		is_valid_map_char(char c);
+//check_parsing.c
+int		check_texture(t_texture *txtr);
+int		check_map(t_map *map);
+//free.c
+void	free_array(char **tab);
 
 #endif
