efbfb97e7a7e0c75bf749d121760d2bbb2084cdf
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-07-02 16:00:22 +0200

    add too many arguments message

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:46:48 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/07/02 11:23:16 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/07/02 15:59:52 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ int	main(int argc, char **argv)
 {
 	t_data	data;
 
-	if (argc != 2)
+	if (argc > 2)
+		print_error("Error\nToo many arguments", EXIT_FAILURE);
+	if (argc < 2)
 		print_error("Error\n.cub file needed", EXIT_FAILURE);
 	data_constructor(&data, argv[1]);
 	mlx_action(&data);
