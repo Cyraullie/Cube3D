1c428679fe30d0ca1cf237d391027d5e56cb427e
cyraullie <cyril.goldenschue@gmail.com>
2025-07-24 17:42:38 +0200

    add check .cub in path

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:46:48 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/07/24 17:05:35 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/07/24 17:33:36 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,19 @@ check du nom du file.
 Pas de message d'erreur si Identifier apres la map. 
 */
 
+int	check_file_name(char *path)
+{
+	const char	*ext = ".cub";
+	size_t		len_filename;
+	size_t		len_ext;
+
+	len_filename = ft_strlen(path);
+	len_ext = ft_strlen(ext);
+	if (!ft_strcmp(path, ".cub"))
+		return (1);
+	return (!ft_strcmp(path + (len_filename - len_ext), ext) == 0);
+}
+
 /**
  * @brief main function
  * 
@@ -35,12 +48,11 @@ int	main(int argc, char **argv)
 		printf("Error\nToo many arguments\n");
 		exit(EXIT_FAILURE);
 	}
-	if (argc < 2)
+	if (argc < 2  || check_file_name(argv[1]))
 	{
 		printf("Error\n.cub file needed\n");
 		exit(EXIT_FAILURE);
 	}
-	printf("%s\n", argv[1]);
 	//TODO check name of file need to contain .cub but .cub only didnt work
 	data_constructor(&data, argv[1]);
 	mlx_action(&data);
