220a54cf5d2bc11e566311afb5efc626234789a2
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-07-29 13:17:58 +0200

    sort function in header

--- includes/functions.h
+++ includes/functions.h
@@ -6,59 +6,61 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 15:46:40 by ktintim-          #+#    #+#             */
-/*   Updated: 2025/07/29 10:34:41 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/07/29 13:17:38 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FUNCTIONS_H
 # define FUNCTIONS_H
 
-int			check_file(char *path);
-int			add_struct(t_texture *txtr, char *str);
-void		parse_map(int fd, t_data *data, char *old_buf);
-int			get_data(t_data *data, char *buf, int fd);
-int			check_double(t_texture *txtr, char *str);
-int			check_init_map(char **map);
-void		map_data(char **raw_lines, t_data *data);
-void		free_and_close(int fd, char *str);
-void		free_tab_and_close(int fd, char *str, char **tab);
-void		free_buff_array(char **tab, char *buf);
-int			handle_texture_error(t_texture *txtr);
-int			check_path(char *path);
-int			check_color(int color[3]);
-void		check_parsing(t_data *data);
-void		print_error(char *msg, int status, t_data *data);
-void		free_text(t_texture *text, void *mlx);
-int			check_rgb(char *str);
-int			has_all_identifiers(t_identifiers *id);
-int			is_map_line(const char *line);
-int			is_empty_or_whitespace(const char *str);
-
 //parsing.c
 void		parsing(int fd, t_data *data);
+void		parse_map(int fd, t_data *data, char *old_buf);
+int			add_struct(t_texture *txtr, char *str);
 //checking_utils.c
 int			is_valid_map_char(char c);
+int			has_all_identifiers(t_identifiers *id);
+int			check_path(char *path);
+int			handle_texture_error(t_texture *txtr);
+//check_utils.c
+int			check_double(t_texture *txtr, char *str);
+int			check_file(char *path);
+int			get_data(t_data *data, char *buf, int fd);
 //parsing_utils.c
 void		strip_newline(char *str);
 void		get_map_dimensions(char **lines, t_map *map);
+int			check_rgb(char *str);
+int			is_map_line(const char *line);
+void		map_data(char **raw_lines, t_data *data);
+//copy_map.c
 int			copy_map(char **raw_lines, t_map *map);
+int			check_init_map(char **map);
 //check_parsing.c
+void		check_parsing(t_data *data);
 int			check_texture(t_texture *txtr);
 int			check_map(t_map *map);
+int			is_empty_or_whitespace(const char *str);
+int			check_color(int color[3]);
 //free.c
 void		free_array(char **tab);
 void		free_visited_partial(char **visited, int limit);
-void		exit_game(t_data *data);
 int			close_window(void *param);
+void		free_text(t_texture *text, void *mlx);
+void		print_error(char *msg, int status, t_data *data);
+//free_utils.c
+void		free_buff_array(char **tab, char *buf);
+void		free_tab_and_close(int fd, char *str, char **tab);
+void		free_and_close(int fd, char *str);
 //map.c
 void		open_door(t_data *data);
 int			integrity_check(t_map *map);
-int			is_player(char c);
+int			check_neighbors(t_map *map, int x, int y);
+//map_utils.c
+int			flood_fill_from(t_map *map, int x, int y, char **visited);
 int			is_accessible(char c);
 int			is_valid_cell(t_map *map, int x, int y);
 char		**dup_map(t_map *map);
-int			flood_fill_from(t_map *map, int x, int y, char **visited);
-int			check_neighbors(t_map *map, int x, int y);
+int			is_player(char c);
 
 //struct_constructor
 	//game_constructor.c
@@ -73,20 +75,22 @@ void		xpm_img_constructor(t_img *img, char *path, void *mlx);
 	//constructor_utils.c
 int			malloc_data(t_data *data);
 int			malloc_texture(t_texture *texture);
+	//free_protection.c
 void		correct_free(t_data *data);
 
 //raycasting
 	//mlx_action.c
 void		mlx_action(t_data *data);
 long long	get_time(void);
-	//key_handler.c
+	//key_press.c
 int			key_press(int key, void *param);
+	//key_handler.c
 int			key_unpress(int key, void *param);
 int			mouse_move(int x, int y, void *param);
 void		key_pressed(t_data *data);
 double		new_x(double angle, t_data *data);
 double		new_y(double angle, t_data *data);
-	//control.c
+	//colision.c
 int			check_w(t_data *data);
 int			check_s(t_data *data);
 int			check_a(t_data *data);
@@ -98,6 +102,7 @@ void		put_minimap(t_data *data, t_img *scn_img);
 void		draw_square(t_img *img, int square_size, int color);
 void		draw_map(t_data *data, char *map[8]);
 void		draw_line(t_img *img, int color);
+//TODO ^ ? pas utilisee
 void		draw_floor(t_img *scn_img, int color);
 void		draw_ceiling(t_img *scn_img, int color);
 	//rotate_image.c
@@ -111,13 +116,10 @@ int			ft_abs(int a);
 double		ft_fabs(double a);
 	//DDA.c
 void		dda(t_data *data, t_img *scn_img);
-// void	intersection_point(t_dda *vars, t_data *data, double angle);
 	//raycasting.c
 void		raycasting(t_data *data, t_img *scn_img);
 int			rgb_to_hex(int color[3]);
 	//grid_search.c
 void		intersection_point(t_data *data, t_grid *grid, double angle);
 
-//debug
-void		visio_map(char **map, int rows, int cols);
 #endif
