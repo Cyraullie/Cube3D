73941f3dc3db9eaddd48afc55483583af638b196
Kilian Tintim Goncalves <ktintim-@c3r3s3.42lausanne.ch>
2025-08-25 13:38:30 +0200

    norminette

--- srcs/raycasting/no_clip.c
+++ srcs/raycasting/no_clip.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   no_clip.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ktintim <ktintim-@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 13:23:48 by ktintim           #+#    #+#             */
-/*   Updated: 2025/08/25 13:15:14 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/08/25 13:35:44 by ktintim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int	no_clip(t_data *data, int new_x, int new_y)
 	int	pose_x;
 	int	pose_y;
 
-	pose_x = ft_abs((data->character->x_pose+32) / PIXEL);
-	pose_y = ft_abs((data->character->y_pose+32) / PIXEL);
+	pose_x = ft_abs((data->character->x_pose + 32) / PIXEL);
+	pose_y = ft_abs((data->character->y_pose + 32) / PIXEL);
 	if (floor(pose_x) == floor(new_x) || \
 floor(pose_y) == floor(new_y))
 		return (0);
